5dd41ab567b061c3342e863a08a6535af0b0bc65
Steve Evans <sevans@steves.macbook.air.lan>
2025-08-29 12:56:23 -0700

    Simplify New Relic initialization - remove invalid app_name parameter

--- backend/api_handler.py
+++ backend/api_handler.py
@@ -10,9 +10,7 @@ try:
     import newrelic.agent
     # Initialize if environment variables are set
     if os.environ.get('NEW_RELIC_LICENSE_KEY'):
-        # Initialize with app name from environment variable
-        app_name = os.environ.get('NEW_RELIC_APP_NAME', 'EPL-Forecast-APIHandler')
-        newrelic.agent.initialize(app_name=app_name)
+        newrelic.agent.initialize()
         NEW_RELIC_ENABLED = True
     else:
         NEW_RELIC_ENABLED = False
